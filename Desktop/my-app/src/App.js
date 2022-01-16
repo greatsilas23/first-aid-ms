23551e7ba79cde5cb4dfdf3c66364b0cc0f1260e
greatsilas23 <nsilasn@gmail.com>
2022-01-16 09:57:51 +0300

    Dec 2013

---
+++ Desktop/my-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Landing from './Landing.js'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Landing />
+      </div>
+    );
+  }
+}
+
+export default App;
