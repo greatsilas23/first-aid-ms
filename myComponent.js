677a9d50c3b61bca6abcb5c91865d713a702f837
greatsilas23 <nsilasn@gmail.com>
2022-03-07 19:31:14 +0300

    first commit

---
+++ myComponent.js
@@ -0,0 +1,63 @@
+
+import React,{ Component } from 'react'
+
+
+
+import Styles from './Styles.js'
+
+export default myComponent.js = () => {
+
+
+
+	return(
+		<div style={Styles.js}>
+			<button
+
+
+
+
+
+				backgroundColor={Styles.button.bgcolor}
+
+
+				onClick={() => { console.log('click-me')}
+
+}
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+A
+A
+A
+A
+export defailt 
+
