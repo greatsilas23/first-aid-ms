c9b6219b2e26bb1d350a2b06b234c7d083f57c22
greatsilas23 <83508822+greatsilas23@users.noreply.github.com>
2022-04-26 20:50:01 +0300

    Delete Desktop directory

--- Desktop/first-aid/LinearSearch.js
+++
@@ -1,9 +0,0 @@
-const search = (item, query) => {
-    for(let i = 0; i < item.length - 1; i++){
-        if(item[i] === query){
-            return item[i]
-        }
-    }
-}
-
-module.exports.search = search
