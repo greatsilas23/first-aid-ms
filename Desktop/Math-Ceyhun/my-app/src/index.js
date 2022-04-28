bbc3987c1a479f963031131c33accc6f8d96450a
greatsilas23 <83508822+greatsilas23@users.noreply.github.com>
2022-04-28 20:07:53 +0300

    Delete Desktop directory

--- Desktop/Math-Ceyhun/my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-import Styles from './Styles.js';
-
-ReactDOM.render(
-  <App style={Styles.app}/>,
-  document.getElementById('root')
-);
